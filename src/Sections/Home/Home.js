d939c5660035ce97741bab7f146c6cab34be2775
VIGNESHWAR J <jv8110909191@gmail.com>
2023-07-31 21:23:12 +0530

    second commit

--- src/Sections/Home/Home.js
+++ src/Sections/Home/Home.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import { Icon } from '@iconify/react';
 import FrontendAvatar from "../../Asserts/FrontendAvatar.png";
